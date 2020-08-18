b8534f91d5df4274f1e1cce8cc9ca51689fd3164
Alpcan Aydin <alpcan@alpcanaydin.com>
2020-08-18 14:25:27 +0300

    City name generator updated

--- utils/city.js
+++ utils/city.js
@@ -1,144 +1,168 @@
+const { format, eachQuarterOfInterval } = require('date-fns');
+
+const now = new Date();
+const START_DATE = new Date(2008, 1, 1);
+const END_DATE = new Date(now.getFullYear() + 1, 1, 1);
+
 const getAll = () => [
-  { name: 'adana', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'adıyaman', variants: ['adiyaman'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'afyon', variants: ['afyonkarahisar'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'ağrı', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'amasya', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  {
-    name: 'ankara',
-    variants: [],
-    dates: [
-      '2008-01-01..2014-06-01',
-      '2014-06-02..2016-06-01',
-      '2016-06-02..2018-01-01',
-      '2018-01-02..2019-01-01',
-    ],
-  },
-  { name: 'antalya', variants: ['alanya'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'artvin', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'aydın', variants: ['aydin', 'kuşadası', 'kusadasi'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'balıkesir', variants: ['balikesir'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'bilecik', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'bingöl', variants: ['bingol'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'bitlis', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'bolu', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'burdur', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'bursa', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'çanakkale', variants: ['canakkale'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'çankırı', variants: ['cankiri'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'çorum', variants: ['corum'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'denizli', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'diyarbakır', variants: ['diyarbakir'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'edirne', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'elazığ', variants: ['elazig'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'erzincan', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'erzurum', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'eskişehir', variants: ['eskisehir'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'gaziantep', variants: ['antep'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'giresun', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'gümüşhane', variants: ['gumushane'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'hakkari', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'hatay', variants: ['antakya'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'ısparta', variants: ['isparta'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'mersin', variants: ['içel'], dates: ['2008-01-01..2019-01-01'] },
-  {
-    name: 'istanbul',
-    variants: [],
-    dates: [
-      '2008-01-01..2011-01-01',
-      '2011-01-02..2012-01-01',
-      '2012-01-02..2013-01-01',
-      '2013-01-02..2013-06-01',
-      '2013-06-02..2014-01-01',
-      '2014-01-02..2014-06-01',
-      '2014-06-02..2014-09-01',
-      '2014-09-02..2015-01-01',
-      '2015-01-02..2015-06-01',
-      '2015-06-02..2016-01-01',
-      '2016-01-02..2016-06-01',
-      '2016-06-02..2017-01-01',
-      '2017-01-02..2017-06-01',
-      '2017-06-02..2018-01-01',
-      '2018-01-02..2019-01-01',
-    ],
-  },
-  { name: 'izmir', variants: [], dates: ['2008-01-01..2016-06-01', '2016-06-02..2019-01-01'] },
-  { name: 'kars', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'kastamonu', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'kayseri', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'kırklareli', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'kırşehir', variants: ['kirsehir'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'kocaeli', variants: ['izmit', 'gebze'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'konya', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'kütahya', variants: ['kutahya'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'malatya', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'manisa', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  {
-    name: 'kahramanmaraş',
-    variants: ['kahramanmaras', 'maraş', 'maras'],
-    dates: ['2008-01-01..2019-01-01'],
-  },
-  { name: 'mardin', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'muğla', variants: ['mugla', 'bodrum', 'marmaris'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'muş', variants: ['mus'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'nevşehir', variants: ['nevsehir'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'niğde', variants: ['nigde'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'ordu', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'rize', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'sakarya', variants: ['adapazarı', 'adapazari'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'samsun', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'siirt', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'sinop', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'sivas', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'tekirdağ', variants: ['tekirdag', 'çorlu'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'tokat', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'trabzon', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'tunceli', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'şanlıurfa', variants: ['sanliurfa', 'urfa'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'uşak', variants: ['usak'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'van,turkey', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'yozgat', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'zonguldak', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'aksaray', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'bayburt', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'karaman', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'kırıkkale', variants: ['kirikkale'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'batman', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'şırnak', variants: ['sirnak'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'bartın', variants: ['bartin'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'ardahan', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'ığdır', variants: ['igdir'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'yalova', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'karabük', variants: ['karabuk'], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'kilis', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'osmaniye', variants: [], dates: ['2008-01-01..2019-01-01'] },
-  { name: 'düzce', variants: ['duzce'], dates: ['2008-01-01..2019-01-01'] },
+  { name: 'adana', variants: [], chunk: false },
+  { name: 'adıyaman', variants: [], chunk: false },
+  { name: 'afyon', variants: ['afyonkarahisar'], chunk: false },
+  { name: 'ağrı', variants: [], chunk: false },
+  { name: 'amasya', variants: [], chunk: false },
+  { name: 'ankara', variants: [], chunk: true },
+  { name: 'antalya', variants: ['alanya', 'kemer', 'kaş'], chunk: false },
+  { name: 'artvin', variants: [], chunk: false },
+  { name: 'aydın', variants: ['kuşadası', 'didim'], chunk: false },
+  { name: 'balıkesir', variants: ['ayvalık'], chunk: false },
+  { name: 'bilecik', variants: [], chunk: false },
+  { name: 'bingöl', variants: [], chunk: false },
+  { name: 'bitlis', variants: [], chunk: false },
+  { name: 'bolu', variants: [], chunk: false },
+  { name: 'burdur', variants: [], chunk: false },
+  { name: 'bursa', variants: [], chunk: false },
+  { name: 'çanakkale', variants: [''], chunk: false },
+  { name: 'çankırı', variants: [], chunk: false },
+  { name: 'çorum', variants: [], chunk: false },
+  { name: 'denizli', variants: [], chunk: false },
+  { name: 'diyarbakır', variants: [], chunk: false },
+  { name: 'edirne', variants: [], chunk: false },
+  { name: 'elazığ', variants: [], chunk: false },
+  { name: 'erzincan', variants: [], chunk: false },
+  { name: 'erzurum', variants: [], chunk: false },
+  { name: 'eskişehir', variants: [], chunk: false },
+  { name: 'gaziantep', variants: ['antep'], chunk: false },
+  { name: 'giresun', variants: [], chunk: false },
+  { name: 'gümüşhane', variants: [], chunk: false },
+  { name: 'hakkari', variants: [], chunk: false },
+  { name: 'hatay', variants: ['antakya', 'iskenderun'], chunk: false },
+  { name: 'ısparta', variants: [], chunk: false },
+  { name: 'mersin', variants: ['içel', 'tarsus'], chunk: false },
+  { name: 'istanbul', variants: [], chunk: true },
+  { name: 'izmir', variants: [], chunk: true },
+  { name: 'kars', variants: [], chunk: false },
+  { name: 'kastamonu', variants: [], chunk: false },
+  { name: 'kayseri', variants: [], chunk: false },
+  { name: 'kırklareli', variants: [], chunk: false },
+  { name: 'kırşehir', variants: ['kirsehir'], chunk: false },
+  { name: 'kocaeli', variants: ['izmit', 'gebze'], chunk: false },
+  { name: 'konya', variants: [], chunk: false },
+  { name: 'kütahya', variants: ['kutahya'], chunk: false },
+  { name: 'malatya', variants: [], chunk: false },
+  { name: 'manisa', variants: [], chunk: false },
+  { name: 'kahramanmaraş', variants: ['maraş'], chunk: false },
+  { name: 'mardin', variants: [], chunk: false },
+  { name: 'muğla', variants: ['bodrum', 'marmaris', 'datça', 'fethiye'], chunk: false },
+  { name: 'muş', variants: [], chunk: false },
+  { name: 'nevşehir', variants: ['kapadokya', 'ürgüp'], chunk: false },
+  { name: 'niğde', variants: [], chunk: false },
+  { name: 'ordu', variants: [], chunk: false },
+  { name: 'rize', variants: [], chunk: false },
+  { name: 'sakarya', variants: ['adapazarı'], chunk: false },
+  { name: 'samsun', variants: [], chunk: false },
+  { name: 'siirt', variants: [], chunk: false },
+  { name: 'sinop', variants: [], chunk: false },
+  { name: 'sivas', variants: [], chunk: false },
+  { name: 'tekirdağ', variants: ['çorlu'], chunk: false },
+  { name: 'tokat', variants: [], chunk: false },
+  { name: 'trabzon', variants: [], chunk: false },
+  { name: 'tunceli', variants: [], chunk: false },
+  { name: 'şanlıurfa', variants: ['urfa'], chunk: false },
+  { name: 'uşak', variants: [], chunk: false },
+  { name: 'van,turkey', variants: [], chunk: false },
+  { name: 'yozgat', variants: [], chunk: false },
+  { name: 'zonguldak', variants: [], chunk: false },
+  { name: 'aksaray', variants: [], chunk: false },
+  { name: 'bayburt', variants: [], chunk: false },
+  { name: 'karaman', variants: [], chunk: false },
+  { name: 'kırıkkale', variants: [], chunk: false },
+  { name: 'batman', variants: [], chunk: false },
+  { name: 'şırnak', variants: [], chunk: false },
+  { name: 'bartın', variants: ['amasra'], chunk: false },
+  { name: 'ardahan', variants: [], chunk: false },
+  { name: 'ığdır', variants: [], chunk: false },
+  { name: 'yalova', variants: [], chunk: false },
+  { name: 'karabük', variants: [], chunk: false },
+  { name: 'kilis', variants: [], chunk: false },
+  { name: 'osmaniye', variants: [], chunk: false },
+  { name: 'düzce', variants: [], chunk: false },
 ];
 
 const filter = name => getAll().filter(c => c.name === name || c.variants.includes(name));
 
-const createSearchCombinations = city => {
-  const variants = [city.name, ...city.variants];
+const formatDate = date => format(date, 'yyyy.MM.dd');
 
-  return variants.reduce(
-    (prev, cur) => [...prev, ...city.dates.map(d => `location:${cur} created:${d}`)],
-    [],
-  );
-};
+const dateIntervalToRange = interval => {
+  const dates = [];
 
-const toCapitalCase = name => {
-  if (name === 'van,turkey') {
-    return 'Van';
+  // eslint-disable-next-line no-plusplus
+  for (let i = -1; i < interval.length; i++) {
+    if (i === interval.length - 2) {
+      break;
+    }
+
+    dates.push(`${formatDate(interval[i + 1])}..${formatDate(interval[i + 2])}`);
   }
 
-  const [head, ...tail] = name.split('');
-
-  return `${head.toLocaleUpperCase('tr-TR')}${tail.join('')}`;
+  return dates;
 };
+
+const convertToEnglishLetters = name =>
+  name
+    .replace(/ğ/gim, 'g')
+    .replace(/ü/gim, 'u')
+    .replace(/ş/gim, 's')
+    .replace(/ı/gim, 'i')
+    .replace(/ö/gim, 'o')
+    .replace(/ç/gim, 'c');
+
+const createVariants = city =>
+  [city.name, ...city.variants].map(n => ({ name: n, chunk: city.chunk }));
+
+const createTurkishLetterCombinations = (prev, cur) => {
+  const names = [cur.name];
+  const englishVersion = convertToEnglishLetters(cur.name);
+
+  if (cur.name !== englishVersion) {
+    names.push(englishVersion);
+  }
+
+  return [...prev, ...names.map(n => ({ name: n, chunk: cur.chunk }))];
+};
+
+const createDates = (prev, cur) => {
+  if (!cur.chunk) {
+    return [
+      ...prev,
+      { name: cur.name, dates: [`${formatDate(START_DATE)}..${formatDate(END_DATE)}`] },
+    ];
+  }
+
+  const interval = eachQuarterOfInterval({
+    start: START_DATE,
+    end: END_DATE,
+  });
+
+  return [...prev, { name: cur.name, dates: dateIntervalToRange(interval) }];
+};
+
+const createSearchTerms = (prev, cur) => [
+  ...prev,
+  ...cur.dates.map(d => `location:${cur.name} created:${d}`),
+];
+
+const createSearchCombinations = city =>
+  createVariants(city)
+    .reduce(createTurkishLetterCombinations, [])
+    .reduce(createDates, [])
+    .reduce(createSearchTerms, []);
 
 module.exports = {
   filter,
   getAll,
   createSearchCombinations,
-  toCapitalCase,
+  createVariants,
+  createTurkishLetterCombinations,
+  createDates,
+  createSearchTerms,
 };
